1cfddf9c561ec08cc5d32d3538a023d63f6c5966
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-11-07 12:39:18 -0600

    added signup changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+// import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -20,28 +20,26 @@ const SignupForm = () => {
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+    console.log(userFormData);
 
     // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+    // const form = event.currentTarget;
+    // if (form.checkValidity() === false) {
+    //   event.preventDefault();
+    //   event.stopPropagation();
+    // }
 
     try {
-      const response = await createUser(userFormData);
+      const {data} = await createUser({
+        variables: { ...userFormData },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
     }
+
+
 
     setUserFormData({
       username: '',
