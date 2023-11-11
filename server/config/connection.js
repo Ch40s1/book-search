8409e359da562c807545e8cb94d442681d663daa
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-11-11 17:45:48 -0600

    added test connection for atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://xxdanielzxx826:hdxd5u2zHQ4GhvTc@cluster0.qdorelp.mongodb.net/');
 
 module.exports = mongoose.connection;
