74f8739c3c9c768b6f2f2397b1c7105eceb88050
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-11-13 19:29:00 -0600

    added new script

--- server/server.js
+++ server/server.js
@@ -28,6 +28,9 @@ const startApolloServer = async () => {
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get("*", function (req, res) {
+      res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+    });
   }
 
   // app.use(routes);
